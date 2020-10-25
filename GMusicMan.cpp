3b43af4e7233b07e7771d8bd0c9db9aff2a8b193
JonnyKelso <redrummyuk@gmail.com>
2020-10-25 22:19:53 +0000

    Now only gathers list of missing files from csv files and searches external locations for matches. Prompts for accepting a match then renames csv file to discount it from further processing

--- GMusicMan.cpp
+++ GMusicMan.cpp
@@ -17,8 +17,17 @@ struct Library
     std::vector<wstring> missing_files;
     std::vector<wstring> ext_library;
     std::vector<wstring> found_files;
+    std::vector<wstring> error_files;
 };
 string data_path = "";
+std::string getMetaData(std::filesystem::path& path);
+int process_args(int arg_count, char* arg_array[]);
+void buildDictionaryOfExistingMusic(std::vector<wstring>& library);
+void findFilenameInExtLibrary(std::unique_ptr<Library>& library);
+void writeVectorToFile(std::string filename, Library& library);
+
+
+
 int process_args(int arg_count, char* arg_array[])
 {
     if(arg_count == 1)
@@ -44,33 +53,42 @@ void buildDictionaryOfExistingMusic(std::vector<wstring>& library)
     std::wifstream ext_lib_file("sumo_mp3.txt");
     if(ext_lib_file.is_open())
     {
-
-        cout << "Opened library file" << endl;
         wstring line;
         while(ext_lib_file.good())
         {
             std::getline(ext_lib_file, line);
             library.push_back(line);
         }
-        cout << "Read " << library.size() << " files from external library list" << endl;
     }
     else
     {
         cout << "Couldn't read library file" << endl;
     }
-
-        
 }
-void findFilenameInExtLibrary(std::vector<wstring>& ext_library, std::vector<wstring>& missing, std::vector<wstring>& found)
+void findFilenameInExtLibrary(std::unique_ptr<Library>& library)
 {
-    for(auto look_for_str : missing)
+   /* std::filesystem::path found_copies(data_path + "\found_copies");
+    if(!std::filesystem::exists(found_copies))
+    {
+        if(!std::filesystem::create_directory(found_copies))
+        {
+            cout << "Couldn't create a new directory for fouind copies of files." << endl;
+            return;
+        }
+    }*/
+    int remaining = library->missing_files.size();
+    for(auto look_for_str : library->missing_files)
     {   
         std::filesystem::path look_for_path(look_for_str);
-
-        std::wcout << "Looking for [" << look_for_path.stem() << "]";
+        cout << "----------------------------------------------------" << endl;
+        cout << remaining << " remaining." << endl;
+        cout << "* " << getMetaData(look_for_path) << endl;
+        //std::wcout << "* " << look_for_path.stem() << endl; 
         bool found_file = false;
-        for(auto line : ext_library)
+        int file_count = 0;
+        for(auto line : library->ext_library)
         {
+            file_count++;
             std::filesystem::path path(line);
             if(path.filename().wstring().find(look_for_path.stem().wstring().substr(0,10)) != wstring::npos)
             {
@@ -79,22 +97,95 @@ void findFilenameInExtLibrary(std::vector<wstring>& ext_library, std::vector<wst
                     cout << "found file AGAIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
                     std::cin.get();
                 }
-                found.push_back(line);
-                found_file = true;
-                std::wcout << "\n\t.......... Found: " << line << '\n';
+                std::wcout << "* " << look_for_path.stem() << endl; 
+                std::wcout << "-> " << path.filename().wstring() << '\n';
+                std::wcout << "-> (" << file_count << ") Found: " << line << '\n';
+                
                 string accept;
                 cout << "Do you want to accept this match?";
                 getline (std::cin, accept);
                 if(accept == "Y" || accept == "y")
                 {
-                    cout << "accepted." << endl;
+                    library->found_files.push_back(line);
+                    found_file = true;
+                    std::filesystem::path new_path = look_for_path;
+                    new_path += ".found";
+                    //cout << "Renaming: " << look_for_path << " to " << new_path << endl; 
+                    std::filesystem::rename(look_for_path, new_path);
+                    if(std::filesystem::exists(new_path))
+                    {
+                        cout << "Renamed to " << new_path << endl;
+                    }
+                    else
+                    {
+                        cout << "Error: rename FAILED" << endl;
+                    }
+                    remaining--;
+                    break;
+                }
+                else if(accept == "S" || accept == "s")
+                {
+                    // skip
+                    break;
+                }
+                else if(accept == "D" || accept == "d")
+                {
+                    // don't care
+                    std::filesystem::path new_path = look_for_path;
+                    new_path += ".deleteme";
+                    std::filesystem::rename(look_for_path, new_path);
+                    if(std::filesystem::exists(new_path))
+                    {
+                        cout << "Renamed to " << new_path << endl;
+                    }
+                    else
+                    {
+                        cout << "Error: rename FAILED" << endl;
+                    }
+                    remaining--;
+                    break;
                 }
             }
         }
-        cout << '\n';
+        if(!found_file)
+        {
+            library->error_files.push_back(look_for_str);
+        }
     }
-    cout << "Found " << found.size() << " files" << endl;
+    cout << "Found " << library->found_files.size() << " files" << endl;
+    cout << "Did not find " << library->error_files.size() << " files" << endl;
 
+}
+std::string getMetaData(std::filesystem::path& path)
+{
+    std::ifstream meta_file(path);
+    std::string header;
+    std::string data;
+    if(meta_file.is_open())
+    {
+        std::getline(meta_file, header);
+        std::getline(meta_file, data);
+    }
+    else
+    {
+        cout << "Couldn't open metadata file: " << path << endl;
+    }
+    return data;
+}
+void writeVectorToFile(std::string filename, Library& library)
+{
+    std::ofstream outFile(filename);
+    if(outFile.is_open())
+    {
+        for(auto line : library.error_files)
+        {
+            std::wcout << line << '\n';
+        }
+    }
+    else
+    {
+        cout << "Could not open outout file: " << filename << endl;
+    }
 }
 int main(int argc, char* argv[])
 {
@@ -138,5 +229,6 @@ int main(int argc, char* argv[])
     {
         cout << "Some files not read!!!!!!!!!" << endl;
     }
-    findFilenameInExtLibrary(myLibrary->ext_library, myLibrary->missing_files, myLibrary->found_files);
+    //findFilenameInExtLibrary(myLibrary->ext_library, myLibrary->missing_files, myLibrary->found_files);
+    findFilenameInExtLibrary(myLibrary);
 }
